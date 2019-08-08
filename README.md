9575430151737006abd14ff6601540d695fd7af2
monsterhxw <770342643@qq.com>
2019-08-08 23:16:40 +0800

    fea(ThreadedBinaryTree, README.md): added C program to Implement Threaded Binary Tree Algorithm.
    added C program to Implement Threaded Binary Tree Algorithm and updated README.md file.

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 - Pre-Order Traversal In Binary Tree Algorithm
 	
 	[C program for pre-order traversal in binary tree Algorithm](https://github.com/monsterhxw/Data-Structures-And-Algorithms-Practice/blob/master/PreOrderTraversalInBinaryTreeAlgorithm/main.c)
+
+- Threaded Binary Tree Algorithm
+	
+	[C program for Threaded Binary Tree Algorithm](https://github.com/monsterhxw/Data-Structures-And-Algorithms-Practice/blob/master/ThreadedBinaryTree/main.c)
---
+++ ThreadedBinaryTree/main.c
@@ -0,0 +1,95 @@
+// C program to Implement Threaded Binary Tree Algorithm.
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElemType;
+
+// 线索存储标志位
+// Link(0)   表示指向左右孩子的指针
+// Thread(1) 表示指向前驱后继的线索
+typedef enum {
+    LINK,
+    THREAD
+} PointerTag;
+
+typedef struct ThreadedBinaryNode {
+    ElemType data;
+    struct ThreadedBinaryNode *lChild, *rChild;
+    PointerTag lTag, rTag;
+} ThreadedBinaryNode, *ThreadedBinaryTree;
+
+// 全局变量，始终指向刚刚访问过的结点
+ThreadedBinaryTree pre;
+
+// 根据前序遍历的方式来创建二叉树
+void constructBinaryTreeFromPreOrder(ThreadedBinaryTree *tree) {
+    char c;
+    scanf("%c", &c);
+    if ('_' == c) {
+        *tree = NULL;
+    } else {
+        *tree = (ThreadedBinaryNode *) malloc(sizeof(ThreadedBinaryNode));
+        (*tree)->data = c;
+        (*tree)->lTag = LINK;
+        (*tree)->rTag = LINK;
+        constructBinaryTreeFromPreOrder(&(*tree)->lChild);
+        constructBinaryTreeFromPreOrder(&(*tree)->rChild);
+    }
+}
+
+// 中序遍历线索化
+void InOrderTraversal(ThreadedBinaryTree tree) {
+    if (tree) {
+        // 递归左孩子线索化
+        InOrderTraversal(tree->lChild);
+
+        // 如果该结点没有左孩子，设置 lTag 为 THREAD，并把 lChild 指向刚刚访问的结点
+        if (!tree->lChild) {
+            tree->lTag = THREAD;
+            tree->lChild = pre;
+        }
+        // 如果 pre 结点无右孩子，设置 pre 的 rTag 为 THREAD，并将 rChild 指向 T 作为 pre 的后继
+        if (!pre->rChild) {
+            pre->rTag = THREAD;
+            pre->rChild = tree;
+        }
+
+        pre = tree;
+
+        // 递归右孩子线索化
+        InOrderTraversal(tree->rChild);
+    }
+}
+
+
+void InOrderThreading(ThreadedBinaryTree *p, ThreadedBinaryTree T) {
+    *p = (ThreadedBinaryNode *) malloc(sizeof(ThreadedBinaryNode));
+    (*p)->lTag = LINK;
+    (*p)->rTag = THREAD;
+    (*p)->rChild = *p;
+    // 空二叉树，p 的 lChild 指向自己本身
+    if (!T) {
+        (*p)->lChild = *p;
+    } else {
+        // 如果不是空二叉树，p 的 lChild 指向 T
+        (*p)->lChild = T;
+        // 将 p 赋值给 pre
+        pre = *p;
+        // 中序遍历线索化
+        InOrderTraversal(T);
+        pre->rChild = *p;
+        pre->lTag = THREAD;
+        (*p)->rChild = pre;
+    }
+}
+
+int main() {
+    ThreadedBinaryTree T = NULL;
+    ThreadedBinaryTree P = NULL;
+    // "ABDH__I__EJ___CF__G__"
+    printf("请输入 'ABDH__I__EJ___CF__G__'\n");
+    printf("\n");
+    constructBinaryTreeFromPreOrder(&T);
+    InOrderThreading(&P, T);
+    return 0;
+}
